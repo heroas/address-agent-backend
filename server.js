4885ec1ec97e54909e536f67681f8f28aa04e16e
Heroas <heroas@protagonist.xyz>
2018-02-28 18:49:08 -0500

    Using heroku port env

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ var cors = require('cors');
 
 const app = express();
 app.use(cors());
-const port = 8001;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 require('./app/routes')(app);
+
 app.listen(port, () => {
   console.log('Living on port ' + port);
 });

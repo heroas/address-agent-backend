6ebfb601e3fdf98fe0141725e0a13fb84508b8ac
Heroas <heroas@protagonist.xyz>
2018-02-28 19:16:04 -0500

    Ripple support

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,10 @@
 const eth_route = require('./eth_route');
 const btc_route = require('./btc_route');
+const xrp_route = require('./xrp_route');
+
 
 module.exports = function(app) {
   eth_route(app);
   btc_route(app);
+  xrp_route(app);
 };

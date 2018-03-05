8dda70d1fdbd1cc8a18f0ba31592a3cc67f8630f
Heroas <heroas@protagonist.xyz>
2018-03-04 21:32:43 -0500

    Bitcoin api for now

--- app/routes/btc_route.js
+++ app/routes/btc_route.js
@@ -1,19 +1,21 @@
 module.exports = function(app) {
 
+  var blockexplorer = require('blockchain.info/blockexplorer')
   var priceFetcher = require('./price_fetcher');
-  var blockexplorer = require('blockexplorer')
 
   app.post('/getBtcAmtFromAddress', (req, res) => {
-    var balance = blockexplorer.balance(req.body.address)
-    balance.then(data => {
-      var satsToBTC = data / 1e8;
+    var balance = blockexplorer.getAddress(req.body.address)
 
+    balance.then(data => {
+      var satsToBTC = data.final_balance / 1e8;
+
+      console.log(data.final_balance);
       priceFetcher.fetchCoinPrice(req.body.coinName)
         .then(response => {
           var totalAmt = (response * satsToBTC).toFixed(2);
           var result = {"worth": totalAmt, "balance": satsToBTC.toFixed(2)}
 
-          res.json(totalAmt)
+          res.json(result)
         })
     })
   })
